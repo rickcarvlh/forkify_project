c14f9954c2b61aceb0a4a03de1052b7f12135755
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-05 17:24:05 +0000

    First Api Calls finished

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -2,12 +2,20 @@ import axios from 'axios';
 
 async function getResults(query) {
     const key = '2670d4c83d50b0c808cb22448ea081ab';
-    // * save to const result and wait the result
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(res);
+    try {
+        // * save to const result and wait the result
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        // only logging the recipes
+        console.log(recipes);
+
+    } catch (error) {
+        alert(error);
+    }
 }
 
-getResults();
+// it has to be a string
+getResults('pizza');
 
 
 

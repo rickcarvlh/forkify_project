ac4d89acad06c17df237690dffd88f54ae4b30d0
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2018-12-05 18:14:26 +0000

    Building the search model

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,24 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-    const key = '2670d4c83d50b0c808cb22448ea081ab';
-    try {
-        // * save to const result and wait the result
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        // only logging the recipes
-        console.log(recipes);
-
-    } catch (error) {
-        alert(error);
-    }
-}
-
-// it has to be a string
-getResults('pizza');
-
-
-
-// api key
-// 2670d4c83d50b0c808cb22448ea081ab
-// https://www.food2fork.com/api/search
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
--- starter/src/js/models/Search.js
+++ starter/src/js/models/Search.js
@@ -1,4 +1,23 @@
+import axios from 'axios';
 
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+    // ! asyncrounous method
+    // no parameter we will always read from the object "this"
+    async getResults() {
+        const key = '2670d4c83d50b0c808cb22448ea081ab';
+        try {
+            // * save to const result and wait the result
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            // const recipes = res.data.recipes; -> old
+            this.result = res.data.recipes;
+            // only logging the recipes
+            // console.log(this.result);
 
-
-
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
